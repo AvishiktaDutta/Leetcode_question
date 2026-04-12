b6bebeb1f525a046dddffd69904a6123953d9cb1
Avishikta Dutta <avishiktadutta8@gmail.com>
2026-04-12 08:41:58 +0530

    Time: 1 ms (99.98%), Space: 47.3 MB (9.66%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int value = 0;
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            value = value ^ nums[i];
+        int ans = 0; 
+        for(int i = 0; i<nums.length; i++){
+            ans = ans ^ nums[i];
         }
-        return value;
+        return ans;
     }
 }
